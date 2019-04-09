3798b8a6b640dc1ae2149cf63a300254b26fd4b6
Dindin Hernawan <dindin_hernawan@yahoo.com>
2019-04-10 01:45:27 +0700

    Pembaharuan Script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,16 @@
+#!/usr/bin/env bash
+# Meregister Subcripe RHEL Baru Pertamakali
 subscription-manager register --username <username> --password <password> --auto-attach
-yum groupinstall 'Developer Tools' 'gdm' 'X Window System' 'Xfce'
-systemctl enable graphical.target
-systemctl enable multi-user.target
-systemctl set-default graphical.target
-reboot
-
+# Meregister Subcripe RHEL Timpa dengan Yang Baru
+subscription-manager register --username <username> --password <password> --auto-attach --force
+# Memasang GUI Gnome 
 yum groupinstall gnome-desktop x11 fonts
+# Memasang Developer Tools
+yum groupinstall 'Developer Tools'
+# Menjadikan GUI sebagai deefault
 systemctl set-default graphical.target
 systemctl start graphical.target
+systemctl enable graphical.target
+systemctl enable multi-user.target
+# Merestar System
+reboot
