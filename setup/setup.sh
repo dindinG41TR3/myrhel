07783cfa9ca62d9ac53b2eb54985f6c0877c9abc
Dindin Hernawan <dindin_hernawan@yahoo.com>
2019-04-10 00:14:34 +0700

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,6 @@
 subscription-manager register --username <username> --password <password> --auto-attach
 yum groupinstall 'X Window System' 'Xfce'
+systemctl enable graphical.target
+systemctl enable multi-user.target
 systemctl set-default graphical.target
+reboot
